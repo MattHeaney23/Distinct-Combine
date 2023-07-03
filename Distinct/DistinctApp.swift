3a36904697eee8381f1351c6cb207e5bdbb85f29
Matt Heaney <main@mattheaney.dev>
2023-07-03 16:20:51 +0100

    Starting Project

---
+++ Distinct/DistinctApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistinctApp.swift
+//  Distinct
+//
+//  Created by Matt Heaney on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DistinctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
